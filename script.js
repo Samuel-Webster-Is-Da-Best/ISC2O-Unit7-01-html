67dc5d86aa98f2a5ec76f41cf21298d4f3a92a8c
SamuelWebsterIs <>
2021-06-08 14:20:23 +0000

    fixes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let numb = Math.floor(Math.random() * 10 + 1)
-document.getElementById("button").addEventListener("click", guess)
+document.getElementById('button').addEventListener('click', guess)
 
 function guess () {
-  let answer = document.getElementById("answer").value
-  let theirs = parseInt (answer)
-  if (theirs == numb) {
+  const answer = document.getElementById('answer').value
+  const theirs = parseInt(answer)
+  if (theirs === numb) {
     alert('you got it!')
-    document.getElementById("answer").value = ''
+    document.getElementById('answer').value = ''
     numb = Math.floor(Math.random() * 10 + 1)
   } else {
     alert('try again!')
-    document.getElementById("answer").value = ''
+    document.getElementById('answer').value = ''
   }
 }
